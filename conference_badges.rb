b17e7ce13fa87a7eaa8d3f0021f7888babf4a38d
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-05 12:40:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,7 @@ arr = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
 def badge_maker(array)
   array.each do |name|
     return "Hello, my name is #{name}."
+  end
 end
 
 badge_maker(arr)
