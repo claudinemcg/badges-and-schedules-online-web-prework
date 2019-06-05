3cef712d53c5b9bb0ef64638eb481640e1a49b79
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-05 13:01:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,24 +8,28 @@ end
 puts badge_maker(arr)
 
 =end
-def badge_maker(name)
+def badge_maker(name)                           # one badge maker
   return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(arr)
   new_arr = []
   arr.each do |name|
-    message = "Hello, my name is #{name}."
+    message = "Hello, my name is #{name}."      # multiple badge maker
     new_arr << message
   end
   return new_arr
 end
 
-def assign_rooms(arr)
+def assign_rooms(arr)                           # multiple badge and room assignemnt maker
 new_arr = []
   arr.each_with_index do |name, index|
     message = "Hello, #{name}! You'll be assigned to room #{index+1}!"
     new_arr << message
   end
   return new_arr
+end
+
+def printer
+  puts assign_rooms(arr)
 end
