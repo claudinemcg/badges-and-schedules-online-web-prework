763925f1cd4dd90b63c910df9b5b0635bfe1f5a3
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-05 12:39:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,8 @@
-array = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
+arr = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
 
 def badge_maker(array)
   array.each do |name|
     return "Hello, my name is #{name}."
 end
+
+badge_maker(arr)
